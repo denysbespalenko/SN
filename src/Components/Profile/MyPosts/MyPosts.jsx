41312653b2ea208574287f8672ebb668ca853697
denysbespalenko <denis.bespalenko@gmail.com>
2020-03-05 01:01:38 +0200

    1.Messages and dialogs as a components 2. Map for Messages, Dialogs, Posts Arrays 3. All data to index.js as Arrays + props routers

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,16 +2,9 @@ import React from 'react';
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        {id: 1, message: 'Hi how are you? ', likeCount: '15'},
-        {id: 1, message: 'It\'s my first post! ', likeCount: '12'},
-        {id: 1, message: 'WTF! ', likeCount: '12'},
-        {id: 1, message: 'Goaway ', likeCount: '12'},
-
-    ];
-    let postsElements = posts.map(post => <Post message={post.message} likeCount={post.likeCount}/>);
+    let postsElements = props.posts.map(post => <Post message={post.message} likeCount={post.likeCount}/>);
     return (
         <div className={s.MyPosts}>
             <h3>My posts</h3>
